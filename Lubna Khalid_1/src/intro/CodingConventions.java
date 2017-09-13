2dae2c57dfc3f55c2d86fa86640ffa980af8f711
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-09-13 10:58:20 -0400

    Completed the Coding Conventions.

--- Lubna Khalid_1/src/intro/CodingConventions.java
+++ Lubna Khalid_1/src/intro/CodingConventions.java
@@ -12,9 +12,20 @@ public class CodingConventions {
 	//that is because the thing it returns is an instance of  the class
 	* therefore the name of a constructor must always match the name of the class.
 	*/
-	public CodingConventions() {
+	public CodingConventions(String name, int index) {
 		//In Java fields are instantiated in a constructor.
-		name = "Mr.Nockles";
-		description = "is a teacher";
+		// there is a local variable called 'name' and a field called 'name'
+		//this field persists, but the local variable data is destroyed 
+		this.name = name ;
+		//static reference begins with a class name
+		description = introMain.DESCRIPTIONS[index];
+		
+	
+	}
+
+	public void doStuff() {
+		//static method call always there (like your graduating year)
+		System.out.println(name + description);//use a space between your operations
+		System.out.println("The square root of 12 is " + Math.sqrt(12));
 	}
 }
--- Lubna Khalid_1/src/intro/introMain.java
+++ Lubna Khalid_1/src/intro/introMain.java
@@ -2,11 +2,18 @@ package intro;//this is a package declaration
 //this is class header (class declaration)
 public class introMain {
 
+	//constants (public)
+	public static final String[] DESCRIPTIONS = {"is a teacher",
+			"is a human being",
+			"is imaginary"};
+	
 	public static void main(String[] args) {
 		//declaration of a local variable
 		//notice the use of the word 'new'
 		//new must be used to call a construction 
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Mr Nockles",0);
+		//instance method call is a individual example of CodingConventions
+		conventionsInstance.doStuff();  
 	}
 
 }

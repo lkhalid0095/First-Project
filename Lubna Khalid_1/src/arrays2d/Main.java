7a8206a86adb72f7f7da29dee664320fcd3eb4e9
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-10-20 11:45:08 -0400

    2D start

---
+++ Lubna Khalid_1/src/arrays2d/Main.java
@@ -0,0 +1,25 @@
+package arrays2d;
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+	/**
+	 * increases the element in arr at index psn
+	 * ad decrease the elements at psn  - 1 and psn +1,
+	 * IF THEY EXIST.
+	 * THIS idea is another major topic in APCSA
+	 * it is the idea of avoiding an ArrayIndexOutofBoundsException.
+	 * You will see it on quizzes, tests and projects rubrics.
+	 * ALWAYS BE MINDFUL!!
+	 */
+	public static void changeNeighbors(int[] arr, int psn) {
+		arr[psn] = arr[psn] +1;
+		if(psn - 1>= -0);
+		
+		
+		}
+	}
+
+

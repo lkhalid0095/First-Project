3bf3a97417be193d27280839324c33ab37fc2b4d
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-10-31 11:39:54 -0400

    committe

--- Lubna Khalid_1/src/caveExplorer/Inventory.java
+++ Lubna Khalid_1/src/caveExplorer/Inventory.java
@@ -3,62 +3,54 @@ package caveExplorer;
 public class Inventory {
 
 	private String map;
+	
 	public Inventory() {
-		
 		updateMap();
 	}
 
 	public void updateMap() {
-		
 		map = " ";
-		//create the line across top.
-		for(int i = 0; i < CaveExplorer.caves[0].length -1; i++ ) {
-			map += "____";//4 underscores.
-			
+		//create line across top:
+		for(int i = 0; i < CaveExplorer.caves[0].length -1; i++) {
+			map += "____";//4 underscores
 		}
-		map+= "___\n"; //3 underscores, make the corner look symmetrical.
-		for(CaveRoom[] row: CaveExplorer.caves) {
-			//3 rows of text.
-			for(int i = 0; i<3; i++) {
+		map+= "___\n";//3 underscores, makes the corner look symmetrical
+		for(CaveRoom[] row : CaveExplorer.caves) {
+			//3 rows of text
+			for(int i = 0; i < 3; i++) {
 				String text = "";
-				for(CaveRoom cr:row) {
-					//if door is open, leave open.
-					if(cr.getDoor(CaveRoom.WEST) != null && cr.getDoor(CaveRoom.WEST).isOpen()) {
-						text+= " ";
-					} else {
+				for(CaveRoom cr : row) {
+					//if door is open, leave open
+					if(cr.getDoor(CaveRoom.WEST) != null &&
+							cr.getDoor(CaveRoom.WEST).isOpen()) {
+						text += " ";
+					}else {
 						text += "|";
 					}
-					//contents of room depend on what row this is.
-					if(i == 0) {
-						text+= "   "; //3 spaces
-						
-					}
-					else if(i==1) {
+					//contents of room depend on what row this is
+					if(i==0) {
+						text+="   ";//3 spaces
+					}else if(i == 1) {
 						text += " "+cr.getContents()+" ";
-						
-					}
-					else if (i == 2) {
-						//draw space if door to south is open.
-						if(cr.getDoor(CaveRoom.SOUTH) != null && cr.getDoor(CaveRoom.SOUTH).isOpen()) {
-							text+="   ";//3 spaces.
-						}
-						else {
-							text += "____";
+					}else if(i == 2) {
+						//draw space if door to south is open
+						if(cr.getDoor(CaveRoom.SOUTH) != null && 
+						cr.getDoor(CaveRoom.SOUTH).isOpen()){
+							text+="   ";//3 spaces
+						}else {
+							text += "___";
 						}
 					}
-				}
-				//last caveroom in row.
-				text += "|";
-				map += text + "\n";
+				}//last caveroom in row
+				text+="|";
+				map += text +"\n";
 			}
 		}
-		
 	}
 
 	public String getDescription() {
-
 		return map;
-		//return "You have nothing in your inventory.";
+//		return "You have nothing in your inventory.";
 	}
 
 }
---
+++ Lubna Khalid_1/src/caveExplorer/NPC.java
@@ -0,0 +1,26 @@
+package caveExplorer;
+
+public class NPC {
+/**
+ * NPC means non player.
+ */
+	public NPC() {
+		
+	}
+
+	public void interact() {
+		
+		
+	}
+
+	public boolean isActive() {
+		
+		return false;
+	}
+
+	public String getInactiveDescription() {
+		
+		return null;
+	}
+
+}

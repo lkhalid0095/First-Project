fbd6f066212457e08eab8ebf7ef1d281be5e1d70
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-12-05 11:05:13 -0500

    tower

--- Lubna Khalid_1/src/search/TowerOfHanoi.java
+++ Lubna Khalid_1/src/search/TowerOfHanoi.java
@@ -4,7 +4,7 @@ public class TowerOfHanoi {
 
 	public static void main(String[] args) {
 		
-		int n = 3;
+		int n = 5;
 		String start = "A";
 		String helper = "B";
 		String end = "C";
@@ -14,11 +14,11 @@ public class TowerOfHanoi {
 	public static void towerSolution(int n, String start, String helper,
 			String end) {
 		if(n == 1){
-			System.out.print("Move " +start + " to " + end  );
+			System.out.println(start + " to " + end  );
 		}
 		else {
 			towerSolution(n-1 ,start,end,helper);
-			System.out.println(" move "+start + " to " + end);
+			System.out.println(start + " to " + end);
 			towerSolution(n-1, helper, start, end);
 		}
 	}
